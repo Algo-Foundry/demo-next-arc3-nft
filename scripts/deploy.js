80ac96302913fd7efe986e2027869c0d8cc78e13
Charlie Kong <charlie@undercurrent.tech>
2023-04-25 18:22:28 +0800

    deploy arc3 nfts

---
+++ scripts/deploy.js
@@ -0,0 +1,109 @@
+import pinataSDK from "@pinata/sdk";
+import * as fs from "fs";
+import * as path from "path";
+import { fileURLToPath } from "url";
+import * as mime from "mime-types";
+import * as dotenv from "dotenv";
+dotenv.config({ path: "./.env.local" });
+import * as algotxns from "../src/algorand/index.js";
+import { getAlgodClient } from "../src/clients/index.js";
+import algosdk from "algosdk";
+
+const pinata = pinataSDK(process.env.PINATA_API_KEY, process.env.PINATA_API_SECRET);
+
+const network = process.env.NEXT_PUBLIC_NETWORK || "SandNet";
+const algodClient = getAlgodClient(network);
+
+// get creator account
+const deployer = algosdk.mnemonicToSecretKey(process.env.DEPLOYER_MNEMONIC);
+
+(async () => {
+  // check pinata connection
+  const response = await pinata.testAuthentication();
+  if (!response) {
+    console.log("Unable to authenticate with Pinata");
+    return;
+  }
+
+  // list of asset names
+  const assetNames = ["ACS Corgi", "ACS Shiba Inu"];
+
+  // read directory
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+  const sourcePath = path.join(__dirname, "../assets/nfts/");
+  const assets = fs.readdirSync(sourcePath).map((file, index) => {
+    const asset = {
+      index: index + 1, // 1-based index
+      name: `${assetNames[index]} #${index + 1}`, // e.g Corgi #1
+      description: `Asset ${index + 1}/${assetNames.length}`,
+      image_mimetype: mime.lookup(file),
+      file: file,
+    };
+    return asset;
+  });
+  console.log(assets);
+
+  // pin content directory
+  const folderOptions = {
+    pinataMetadata: {
+      name: "nfts",
+    },
+    pinataOptions: {
+      cidVersion: 0,
+    },
+  };
+
+  const result = await pinata.pinFromFS(sourcePath, folderOptions);
+  console.log("Digital content pinned: ", result);
+
+  // prepare to deploy assets
+  await Promise.all(
+    assets.map(async (asset) => {
+      // construct JSON metadata for each asset
+      const metadata = {
+        name: asset.name,
+        description: asset.description,
+        image: `ipfs://${result.IpfsHash}/${asset.file}`, // update url here for windows users
+        image_mimetype: asset.image_mimetype,
+        properties: {
+          file_url: `https://gateway.pinata.cloud/ipfs/${result.IpfsHash}/${asset.file}`, // update url here for windows users
+          file_url_mimetype: asset.image_mimetype,
+        },
+      };
+
+      // pin metadata
+      const jsonOptions = {
+        pinataMetadata: {
+          name: `${asset.index}-metadata.json`,
+        },
+        pinataOptions: {
+          cidVersion: 0,
+        },
+      };
+
+      const resultMeta = await pinata.pinJSONToIPFS(metadata, jsonOptions);
+      console.log("JSON Metadata pinned: ", resultMeta);
+
+      // ARC3 asset url should end with #arc3 - https://github.com/algorandfoundation/ARCs/blob/main/ARCs/arc-0003.md
+      const preparedAsset = {
+        name: asset.name,
+        url: `ipfs://${resultMeta.IpfsHash}#arc3`,
+      };
+
+      // deploy asset
+      const createNftTxn = await algotxns.getCreateNftTxn(
+        algodClient,
+        deployer.addr,
+        asset.name,
+        false,
+        "ACSNFT",
+        preparedAsset.url
+      );
+      console.log(await algotxns.signAndSubmit(algodClient, [createNftTxn], deployer));
+
+      // write created NFTs to JSON file
+      
+    })
+  );
+})();
--- src/algorand/index.js
+++ src/algorand/index.js
@@ -11,6 +11,40 @@ const getPaymentTxn = async (algodClient, from, to, amount) => {
   });
 }
 
+const getCreateNftTxn = async (algodClient, from, assetName, defaultFrozen, unitName, assetURL) => {
+  const suggestedParams = await algodClient.getTransactionParams().do();
+
+  // txn to create a pure nft
+  return algosdk.makeAssetCreateTxnWithSuggestedParamsFromObject({
+    from,
+    assetName,
+    total: 1,
+    decimals: 0,
+    defaultFrozen,
+    unitName,
+    assetURL,
+    suggestedParams
+  });
+}
+
+const signAndSubmit = async (algodClient, txns, signer) => {
+  // used by backend to sign and submit txns
+  const groupedTxns = algosdk.assignGroupID(txns);
+
+  const signedTxns = groupedTxns.map(txn => txn.signTxn(signer.sk));
+
+  const response = await algodClient.sendRawTransaction(signedTxns).do();
+
+  const confirmation = await algosdk.waitForConfirmation(algodClient, response.txId, 4);
+
+  return {
+    response,
+    confirmation
+  };
+}
+
 export {
-  getPaymentTxn
+  getPaymentTxn,
+  getCreateNftTxn,
+  signAndSubmit
 }
